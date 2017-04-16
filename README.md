8dd655a9b0eb100d8cb90ebdfc149069199db16c
AkrGas <akrem.gassoumi@gmail.com>
2017-04-16 15:57:12 +0100

    updates

---
+++ README.md
@@ -0,0 +1,5 @@
+# A Ruby-on-rails web app
+
+Ror is a ruby framework for development web appliaction 
+
+this project is done during my training on this framework.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Webapp::Application.config.secret_key_base = 'b601ebcf0990747e200a14fd9cbbe38cbff97140f7d8734ce51988877f9b69f853b42a13fa6f43b097d37cd7fa06bd29aed1c7b98028e09b0a117db039778d5a'
